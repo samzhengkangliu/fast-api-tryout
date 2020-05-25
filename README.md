44da22a38fe8c11a717d67fbc285580ac67e253f
Maskz1 <o4d0b@outlook.com>
2020-05-25 00:27:32 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Fast API Tryout
+
+This is my first tryout on Fast API following official tutorial.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install Fast API.
+
+```bash
+pip install fastapi
+pip install uvicorn
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/")
+async def get_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
